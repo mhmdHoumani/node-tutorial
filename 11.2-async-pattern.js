26c68594f173062ee2473a7e6debcd14398d54db
Mhmd Houmani <mhoumani742@gmail.com>
2022-04-15 17:39:10 +0300

    A better way in reading and writing files (11.2)

---
+++ 11.2-async-pattern.js
@@ -0,0 +1,31 @@
+const { readFile, writeFile } = require("fs");
+const util = require("util");
+
+const readFilePromise = util.promisify(readFile);
+const writeFilePromise = util.promisify(writeFile);
+
+const start = async () => {
+  try {
+    const first = await readFilePromise("./content/first.txt", "utf-8");
+    const second = await readFilePromise("./content/second.txt", "utf-8");
+    await writeFilePromise(
+      "./content/result-mind-grenade.txt",
+      `THIS IS AWESOME : ${first}, ${second}`
+      //   { flag: "w" }
+    );
+    console.log(first, second);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
+
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, "utf-8", (err, data) => {
+//       if (err) reject(err);
+//       else resolve(data);
+//     });
+//   });
+// };
